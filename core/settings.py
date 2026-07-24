952119723553f0393b41ff18de6d6d2c26f6a0e3
sallmaxn <tomsalman08@gmail.com>
2026-07-24 13:00:46 +0530

    fix allowed host

--- core/settings.py
+++ core/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = [
-    "on.render.com",
+    "techjays-ai-internship-task1.onrender.com",
     "127.0.0.1",
     "localhost",
 ]
